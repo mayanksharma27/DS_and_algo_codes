5e0ba9d6a014f4ccce3a79d07b4ed884844b290d
mayank sharma <mayanksai.sharma27@gmail.com>
2019-02-15 17:38:15 +0530

    Queue - BFS
    Queue - BFS

---
+++ queue/castle-on-the-grid.cpp
@@ -0,0 +1,118 @@
+//https://www.hackerrank.com/challenges/castle-on-the-grid
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the minimumMoves function below.
+int minimumMoves(vector<string> grid, int startX, int startY, int goalX, int goalY) {
+    vector<vector<int>>visited(grid.size(),vector<int>(grid.size(),-1));
+   // count(grid.size(),vector<int>(grid.size(),-1));
+    int i,j;
+    pair<int,int>p;
+    queue<pair<int,int>>q;
+    q.push(pair<int,int>(startX,startY));
+    visited[startX][startY]= 0;
+    while(!q.empty()){
+        p=q.front();
+        q.pop();
+        for(i=p.first+1;i<grid.size();i++){
+            if(grid[i][p.second] == 'X')break;
+            if(visited[i][p.second] == -1){
+                    q.push(pair<int,int>(i,p.second));
+                    visited[i][p.second]=visited[p.first][p.second]+1;
+            }
+        }
+        for(i=p.first-1;i>=0;i--){
+            if(grid[i][p.second] == 'X')break;
+            if(visited[i][p.second] == -1){
+                    q.push(pair<int,int>(i,p.second));
+                    visited[i][p.second]=visited[p.first][p.second]+1;
+            }
+        }   
+        for(i=p.second+1;i<grid.size();i++){
+            if(grid[p.first][i] == 'X')break;
+            if(visited[p.first][i] == -1){
+                    q.push(pair<int,int>(p.first,i));
+                    visited[p.first][i]=visited[p.first][p.second]+1;
+            }
+        }
+        for(i=p.second-1;i>=0;i--){
+            if(grid[p.first][i] == 'X')break;
+            if(visited[p.first][i] == -1){
+                    q.push(pair<int,int>(p.first,i));
+                    visited[p.first][i]=visited[p.first][p.second]+1;
+            }
+        }            
+    }
+    return visited[goalX][goalY];
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    vector<string> grid(n);
+
+    for (int i = 0; i < n; i++) {
+        string grid_item;
+        getline(cin, grid_item);
+
+        grid[i] = grid_item;
+    }
+
+    string startXStartY_temp;
+    getline(cin, startXStartY_temp);
+
+    vector<string> startXStartY = split_string(startXStartY_temp);
+
+    int startX = stoi(startXStartY[0]);
+
+    int startY = stoi(startXStartY[1]);
+
+    int goalX = stoi(startXStartY[2]);
+
+    int goalY = stoi(startXStartY[3]);
+
+    int result = minimumMoves(grid, startX, startY, goalX, goalY);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
